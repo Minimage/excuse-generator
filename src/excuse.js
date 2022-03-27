149850077de98911e220c27c0670c5faca8c2b5d
Minimage <seancampbell914.com>
2022-03-27 09:33:23 -0400

    javascript is built, Now to make HTML

---
+++ src/excuse.js
@@ -0,0 +1,13 @@
+
+a = ["The", 'A']
+b = ["Devil", "Dog", "Robber", "Kitten"]
+c = ["stole", "ate", "hid", "burried", "ripped"]
+d = ["my homework", "my car", "my money", "my keys"]
+
+
+aa = Math.floor(Math.random() * a.length)
+bb = Math.floor(Math.random() * b.length)
+cc = Math.floor(Math.random() * c.length)
+dd = Math.floor(Math.random() * d.length)
+
+console.log(a[aa] +" "+b[bb] +" "+ c[cc] +" "+ d[dd])
